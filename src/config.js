1b268130baae64f2f5cd9ce1ede922e21e78f350
star-citizen <Christoph07@gmail.com>
2017-11-12 22:06:15 -0500

    updated routes

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 //test api
-export const API_BASE = 'http://localhost:3000/api/v1'
+export const API_BASE = 'https://scotch-vue-shop-api.herokuapp.com/api/v1'
+// export const API_BASE = 'http://localhost:3000/api/v1'
